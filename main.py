b0cc6f73e6c9470f4ef3561ad500fb5e695b8ec5
Mike Veronie <mveronie@gmail.com>
2026-01-08 16:30:10 -0500

    Add test widgets

--- main.py
+++ main.py
@@ -3,20 +3,39 @@ Main file for lanching travtools tkinter gui
 """
 
 import tkinter as tk
+from tkinter import ttk
 
 # main app window
 root = tk.Tk()
 root.title("Travtools App")
 root.configure(background="honeydew2")
 root.minsize(600, 600)
-# root.maxsize(500, 500)
 root.geometry("300x300+50+50")
 
-# add a label widget to root and pack with y-padding
-tk.Label(root, text="Hello, Python GUI!").pack(pady=20)
-tk.Label(root, text="An Image below:").pack()
+# Widgets
+widgets = [
+    tk.Label,
+    tk.Checkbutton,
+    ttk.Combobox,
+    tk.Entry,
+    tk.Button,
+    tk.Radiobutton,
+    tk.Scale,
+    tk.Spinbox,
+]
 
-image = tk.PhotoImage(file="guitar-small.png")
-tk.Label(root, image=image).pack()
+for widget in widgets:
+    try:
+        widget = widget(root, text=widget.__name__)
+    except tk.TclError:
+        widget = widget(root)
+    widget.pack(padx=5, pady=5, fill="x")
+
+# # add a label widget to root and pack with y-padding
+# tk.Label(root, text="Hello, Python GUI!").pack(pady=20)
+# tk.Label(root, text="An Image below:").pack()
+#
+# image = tk.PhotoImage(file="guitar-small.png")
+# tk.Label(root, image=image).pack()
 
 root.mainloop()
