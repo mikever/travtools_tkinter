d81591a90c8f172cd78144f5cd42e1e13f9f5c42
Mike Veronie <mveronie@gmail.com>
2026-01-08 16:12:32 -0500

    Add test photo label to root

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+"""
+Main file for lanching travtools tkinter gui
+"""
+
 import tkinter as tk
 
 # main app window
-window = tk.Tk()
-window.title("Travtools App")
-window.geometry("300x200")
+root = tk.Tk()
+root.title("Travtools App")
+root.configure(background="honeydew2")
+root.minsize(600, 600)
+# root.maxsize(500, 500)
+root.geometry("300x300+50+50")
 
-# add a label widget
-label = tk.Label(window, text="Hello, Python GUI!")
-label.pack(pady=20) # Add the label to the window
+# add a label widget to root and pack with y-padding
+tk.Label(root, text="Hello, Python GUI!").pack(pady=20)
+tk.Label(root, text="An Image below:").pack()
 
-# start the GUI event loop
-window.mainloop()
+image = tk.PhotoImage(file="guitar-small.png")
+tk.Label(root, image=image).pack()
+
+root.mainloop()
