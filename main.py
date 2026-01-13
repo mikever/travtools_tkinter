6aec6d84925d75b090a30b72a714f1184ddc5e56
Mike Veronie <mveronie@gmail.com>
2026-01-13 09:23:08 -0500

    Create entry widget

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Main file for lanching travtools tkinter gui
 """
 
 import tkinter as tk
-from tkinter import ttk
+# from tkinter import ttk
 
 # main app window config
 root = tk.Tk()
@@ -15,13 +15,19 @@ root.geometry("300x300+50+50")
 # Actions
 def selection_changed(event):
     '...'
-    label.config(text=f"{event.widget.get()} selected!")
+    selection = event.widget.curselection()
+    if selection:
+        index = selection[0]
+        label.config(text=f"{event.widget.get(index)} selected!")
+        event.widget.get(index)
+
+listbox = tk.Listbox(root)
+for item in ["One", "Two", "Three"]:
+    listbox.insert(tk.END, item)
+listbox.bind("<<ListboxSelect>>", selection_changed)
+listbox.pack(padx=5, pady=5, fill="both", expand=True)
 
 
-combobox = ttk.Combobox(root, values=["One", "Two", "Three"])
-combobox.set("One")
-combobox.bind("<<ComboboxSelected>>", selection_changed)
-combobox.pack(padx=5, pady=5, fill="x")
 
 label = tk.Label(root, text="One Selected!")
 label.pack(padx=5, pady=5, fill="x")
