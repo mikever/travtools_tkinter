6fe69e5a8e2824589c6475b5e7b4a2775612fcd8
Mike Veronie <mveronie@gmail.com>
2026-03-03 09:48:34 -0500

    create test app layout

--- main.py
+++ main.py
@@ -1,31 +1,66 @@
 """
 Main file for lanching travtools tkinter gui
-
-The Scale widget provides a slide-bar that works much like a Spinbox.
-Rather than displaying the current value numerically, it displays the position of the slider handle along the length of the widget.
 """
 
 import tkinter as tk
+from tkinter import ttk
 
 # main app window config
 root = tk.Tk()
 root.title("Travtools App")
-root.configure(background="honeydew2")
-root.minsize(600, 600)
-root.geometry("300x300+50+50")
+image = tk.PhotoImage(file="guitar-small.png")
 
+# tools frame
+tools_frame = tk.Frame(root, width=200, height=400, bg="skyblue")
+tools_frame.pack(padx=5, pady=5, side=tk.LEFT, fill=tk.Y)
+tk.Label(
+    tools_frame,
+    text="Original Image",
+    bg="skyblue",
+).pack(padx=5, pady=5)
+thumbnail_image = image.subsample(5, 5)
+tk.Label(tools_frame, image=thumbnail_image).pack(padx=5, pady=5)
 
-# Actions
-def value_changed(event):
-    label.config(text=event.widget.get())
+# tools and filters tab
+notebook = ttk.Notebook(tools_frame)
+notebook.pack(expand=True, fill="both")
 
+tools_tab = tk.Frame(notebook, bg="lightblue")
+tools_var = tk.StringVar(value="None")
+for tool in ["Resizing", "Rotating"]:
+    tk.Radiobutton(
+        tools_tab,
+        text=tool,
+        variable=tools_var,
+        value=tool,
+        bg="lightblue",
+    ).pack(anchor="w", padx=20, pady=5)
 
-scale = tk.Scale(root, from_=0, to=10, orient="horizontal")
-scale.bind("<Motion>", value_changed)
-scale.pack(padx=5, pady=5, fill="x")
+filters_tab = tk.Frame(notebook, bg="lightgreen")
+filters_var = tk.StringVar(value="None")
+for filter in ["Blurring", "Sharpening"]:
+    tk.Radiobutton(
+        filters_tab,
+        text=filter,
+        variable=filters_var,
+        value=filter,
+        bg="lightgreen",
+    ).pack(anchor="w", padx=20, pady=5)
 
-# helper label to show the selected value
-label = tk.Label(root, text="0")
-label.pack(padx=5, pady=5, fill="x")
+notebook.add(tools_tab, text="Tools")
+notebook.add(filters_tab, text="Filters")
+
+# Image frame
+image_frame = tk.Frame(root, width=400, height=400, bg="grey")
+image_frame.pack(padx=5, pady=5, side=tk.RIGHT)
+display_image = image.subsample(2, 2)
+tk.Label(
+    image_frame,
+    text="Edited Image",
+    bg="grey",
+    fg="white",
+).pack(padx=5, pady=5)
+tk.Label(image_frame, image=display_image).pack(padx=5, pady=5)
+
 
 root.mainloop()
